fb124b8b2da6c87c78fe3406e2d4436f1c6ed91e
mannakub1 <omliler_man@hotmail.com>
2015-06-30 12:54:51 +0700

    feature user

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ActiveRecord::Base
+	has_secure_password
+	validates_presence_of :email
+	validates_uniqueness_of :email
+	validates_presence_of :password
+	validates_presence_of :password_confirmation
+end
---
+++ app/views/users/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @user, :id, :string, :password_digest, :created_at, :updated_at
